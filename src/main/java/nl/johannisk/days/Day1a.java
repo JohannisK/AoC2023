680cc31e0e1de84b2f320c8efed5a7b55fc037de
Johan Kragt <johankragt@Johans-MBP.localdomain>
2023-12-01 09:49:16 +0100

    Cleanup

--- src/main/java/nl/johannisk/days/Day1a.java
+++ src/main/java/nl/johannisk/days/Day1a.java
@@ -2,15 +2,12 @@ package nl.johannisk.days;
 
 import nl.johannisk.support.ChallengeFile;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 public class Day1a {
     public static void main(String[] args) {
         Integer result = new ChallengeFile("day1.txt")
                 .readLines().stream()
                 .map(e -> e.replaceAll("\\D+", ""))
-                .map(e -> e.substring(0, 1) + e.substring(e.length() - 1, e.length()))
+                .map(e -> e.charAt(0) + e.substring(e.length() - 1))
                 .map(Integer::parseInt)
                 .reduce(0, Integer::sum);
         System.out.println(result);
--- src/main/java/nl/johannisk/days/Day1b.java
+++ src/main/java/nl/johannisk/days/Day1b.java
@@ -10,28 +10,35 @@ import static java.util.Map.entry;
 
 public class Day1b {
 
-    public static final Map<String, String> WRITTEN_NUMBERS = Map.of("one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8","nine","9");
+    public static final Map<String, String> WRITTEN_NUMBERS_TO_INTS_MAP = Map.of(
+            "one", "1",
+            "two", "2",
+            "three", "3",
+            "four", "4",
+            "five", "5",
+            "six", "6",
+            "seven", "7",
+            "eight", "8",
+            "nine","9");
 
     public static void main(String[] args) {
         Integer result = new ChallengeFile("day1.txt")
                 .readLines().stream()
-                .map(e -> replaceNumber(e, (needle,haystack) -> haystack.indexOf(needle)))
-                .map(e -> replaceNumber(e, (needle,haystack) -> haystack.lastIndexOf(needle)))
+                .map(e -> replaceWrittenNumberWithIntegerAtIndex(e, (needle, haystack) -> haystack.indexOf(needle)))
+                .map(e -> replaceWrittenNumberWithIntegerAtIndex(e, (needle, haystack) -> haystack.lastIndexOf(needle)))
                 .map(e -> e.replaceAll("\\D+", ""))
                 .map(e -> e.charAt(0) + e.substring(e.length() - 1))
                 .map(Integer::parseInt)
-                .peek(System.out::println)
                 .reduce(0, Integer::sum);
         System.out.println(result);
     }
 
-    public static String replaceNumber(String s, BiFunction<String, String, Integer> indexer) {
-        return WRITTEN_NUMBERS.keySet().stream()
-                .map(e -> entry(indexer.apply(e, s), e))
+    public static String replaceWrittenNumberWithIntegerAtIndex(String line, BiFunction<String, String, Integer> indexer) {
+        return WRITTEN_NUMBERS_TO_INTS_MAP.keySet().stream()
+                .map(e -> entry(indexer.apply(e, line), e))
                 .filter(e -> e.getKey() != -1)
-                .sorted(Entry.comparingByKey())
-                .findFirst()
-                .map(e -> s.substring(0, e.getKey()) + WRITTEN_NUMBERS.get(e.getValue()) + s.substring(e.getKey() + e.getValue().length()))
-                .orElse(s);
+                .min(Entry.comparingByKey())
+                .map(e -> line.substring(0, e.getKey()) + WRITTEN_NUMBERS_TO_INTS_MAP.get(e.getValue()) + line.substring(e.getKey() + e.getValue().length()))
+                .orElse(line);
     }
 }
